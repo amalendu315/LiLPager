2a06c7f304eec8de07a38c657ce015b394b11974
Amalendu Pandey <84476847+amalendu315@users.noreply.github.com>
2021-09-02 22:09:49 +0530

    LiLPager-setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+// import Cookies from 'universal-cookie';
+import { ChannelContainer, ChannelListContainer } from './components';
+import "./App.css"
+const apiKey = "4r8grhtaurxm";
+
+const client = StreamChat.getInstance(apiKey);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app__wrapper">
+            <Chat client={client} theme="team light">
+                <ChannelListContainer />
+                <ChannelContainer />
+            </Chat>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/ChannelContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ChannelContainer() {
+    return (
+        <div>
+            <h1>ChannelContainer</h1>
+        </div>
+    )
+}
+
+export default ChannelContainer
